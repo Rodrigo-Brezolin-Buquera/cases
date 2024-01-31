be16f3ca7c3c08e6e953438b55b1d5e7ea75ed0c
Rodrigo Brezolin <rodrigobrezolin@MacBook-Air-de-Rodrigo.local>
2024-01-31 15:09:48 -0300

    fazendo o case em vue

---
+++ fullstack-RTBHouse/rtbhouse-client-vue /components/Test.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+  <p>teste</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+}
+</script>
---
+++ fullstack-RTBHouse/rtbhouse-client-vue /pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <main>
+    <p>oi</p>
+    <!-- <Tutorial />  -->
+    <Test />
+  </main>
+</template>
+
+<script>
+// const { data } = useFetch('http://localhost:3003/productsData')
+export default {
+  name: 'IndexPage',
+   async getProducts() {
+   const { data } = await $fetch('http://localhost:3003/productsData');
+    // this.list = data.results;
+    console.log(data);
+  },
+  // data() {
+  //   return {
+  //     list: null,
+  //     loading: false
+  //   };
+  // },
+  // mounted() {
+  //   this.loading = false; // You can update the loading state if needed
+  // },
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
